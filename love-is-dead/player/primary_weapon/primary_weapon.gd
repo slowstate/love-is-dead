00d1da8404d28508b923e79eef3167472e9b1db6
slowstate <slowstatedev@gmail.com>
2026-04-04 22:52:23 +1100

    Add basic arrow, primary weapon foundation

---
+++ love-is-dead/player/primary_weapon/primary_weapon.gd
@@ -0,0 +1,14 @@
+class_name PrimaryWeapon
+extends RigidBody2D
+
+enum Type {
+	Aureal,
+}
+
+var cooldown: float = 0.0
+var speed: float = 0.0
+var max_lifetime: float = 0.0
+
+
+func try_fire(_direction: Vector2) -> bool:
+	return false
